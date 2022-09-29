e3aa5580a9b5385f35e5f8c597dda415e84470f5
Taqinur <taqi1662@gmail.com>
2022-09-30 00:15:15 +0600

    added header

---
+++ src/components/Books/Books.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Books.css';
+
+const Books = () => {
+    return (
+        <div className='body-container'>
+            <div className='card-container'>
+                <h2>This is books</h2>
+            </div>
+            <div className="personal-info">
+                <h5>Personal Info</h5>
+            </div>
+        </div>
+    );
+};
+
+export default Books;
